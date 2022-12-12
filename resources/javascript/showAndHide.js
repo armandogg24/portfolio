dbb1edb4bf130f9e4a8f5ef6b8f85d6f18059d77
armandogg24 <armanditogg@gmail.com>
2022-12-11 20:46:38 -0400

    Portfolio 2.0

--- resources/javascript/showAndHide.js
+++
@@ -1,9 +0,0 @@
-const click = document.querySelector('.click');
-const object1 = document.querySelector('.before');
-const object2 = document.querySelector('.after');
-
-function showAndHide() {
-    object1.style.display = "none";
-    object2.style.display = "flex";
-    click.style.display = "none";
-}
